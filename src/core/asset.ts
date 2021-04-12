e420f3c3f6d0d1bbc861c9e7340e070aa6855c03
RoetsSiebe <sieberoets@hotmail.com>
2021-04-12 14:41:36 +0200

    engine work

---
+++ src/core/asset.ts
@@ -0,0 +1,39 @@
+export class Asset {
+  // properties
+  assetId: number;
+  type: string;
+  assetName:string
+  devices:Array<number>;//id's of the devices coupled with this asset
+  coupledAssets: any; //object with key value pairs (maybe consistsOf, owns rules with a list of items coupled to that property)
+  //properties?
+
+  //constructor
+  constructor(assetId: number, type: string, devices: number[], assets:Array<number>) {
+      this.assetId = assetId;
+      this.type = type;
+      this.devices = devices;
+      this.coupledAssets= assets;
+  }
+
+  //methods
+
+  giveJSONformat(){
+    var obj={
+      __uuid:this.assetId,
+      type:this.type,
+      name:this.assetName,
+      relatedDevices:this.devices,
+      relatedAssets:this.coupledAssets
+    };
+  }
+  toString() {
+      var result = "[" + this.assetId + " " + this.type;
+      // @ts-ignore
+      if (this.devices.size > 0)
+          this.devices.forEach((value) => result = result.concat(" (" + value + ")"));
+      if (this.coupledAssets.size > 0)
+          this.coupledAssets.forEach((value, key) => result = result.concat(" (" + key + ", " + value + ")"));
+      result = result.concat("]");
+      return result;
+  }
+}
--- src/core/engine.ts
+++ src/core/engine.ts
@@ -1,17 +1,17 @@
 import {EventFactory} from "./eventFactory";
 import {RestDriver} from "../drivers/RESTdriver";
 import { Device } from "./device";
+import { Asset } from "./asset";
 const EventBus= require("./event-bus");
+var fs = require('fs');
 
 export class Engine {
   // properties
   devices: Device[];
   assets: Asset[];
   drivers:any;
-  prologEngine: PrologEngine;
+  //prologEngine: PrologEngine;
   EventFactory: EventFactory;
-  
-
   /**
    * @constructor
    */
@@ -21,7 +21,7 @@ export class Engine {
       this.assets = new Array();
       this.addDrivers();
       this.EventFactory=new EventFactory();
-      this.prologEngine = new PrologEngine(this);
+   //   this.prologEngine = new PrologEngine(this);
   }
   private addDrivers() {
     this.drivers["RestDriver"]=new RestDriver();
@@ -32,7 +32,7 @@ export class Engine {
     return this.devices
   }
   getAssets() {
-    return this.prologEngine.getAssets();
+  //  return this.prologEngine.getAssets();
   }
   addDevice(device: Device) {
     this.devices.push(device);
@@ -41,24 +41,43 @@ export class Engine {
 
   addAsset(asset: Asset) {
       this.assets.push(asset);
+
   }
 
-  removeDevice(deviceId: string) {
-      const device = this.devices.find(item => item.configurations.deviceId === deviceId);
+  removeDevice(deviceId: number) {
+      const device = this.devices.find(item => item.deviceId === deviceId);
       if (device != null) {
           this.devices.splice(this.devices.indexOf(device), 1);
       }
   }
   run() {
-    this.prologEngine.run();
+   // this.prologEngine.run();
   }
   saveAssetFile(){
-    this.devices.forEach((device)=>{
-      device.giveJSONformat();
+    var file={};
+    file["device"]=[];
+    this.devices.forEach((device:Device)=>{
+      file["device"].push(device.giveJSONformat());
     })
+    this.assets.forEach((asset:Asset)=>{
+      if(file[asset.type]==undefined){
+        //create subgroup
+        file[asset.type]=[];
+      }
+      file[asset.type].push(asset.giveJSONformat());
+    })
+
+    
   }
   readAssetFile(){
+    var data=JSON.parse(fs.readFileSync('../configurations/assets.json'));
+    for(var key in data){
+      // skip loop if the property is from prototype
+      if (!data.hasOwnProperty(key)) continue;
 
+
+    }
   }
+
 
 }
